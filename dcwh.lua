cd5b0c6933a4928b7b783de7b117932b9193c53b
948265 <111317527+948265@users.noreply.github.com>
2022-12-08 16:00:55 -0300

    Update dcwh.lua

--- dcwh.lua
+++ dcwh.lua
@@ -1,7 +1,8 @@
+a1 = _G.SettingsTable.WhURL
 dothethingy = http_request or request or HttpPost or syn.request
-            dothethingy({Url = _G.SettingsTable.WhURL, Body = game:GetService("HttpService"):JSONEncode({
+            dothethingy({Url = a1, Body = game:GetService("HttpService"):JSONEncode({
             ["embeds"] = {{["title"] = "**Rejoined**",
-            ["description"] = "**Username: **" .. me.Name,
+            ["description"] = "**Username: **" .. game.Players.LocalPlayer.Name,
             ["type"] = "rich",
             ["color"] = tonumber(0x7269da)}}}), 
             Method = "POST", Headers = {
